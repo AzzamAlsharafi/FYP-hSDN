deb95b96c3e33922b006f1736f96413447d4e3fe
Azzam Alsharafi <32015536+AzzamAlsharafi@users.noreply.github.com>
2023-12-15 10:00:59 +0000

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ python3.9 -m venv .venv
 source .venv/bin/activate
 
 git clone https://github.com/faucetsdn/ryu.git
+cd ryu
 git checkout d6cda4f427ff8de82b94c58aa826824a106014c2
-pip install ./ryu
-pip install -r ./ryu/tools/optional-requires
+pip install .
+pip install -r ./tools/optional-requires
