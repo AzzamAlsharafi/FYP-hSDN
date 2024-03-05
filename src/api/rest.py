a4a9d917a481971383e86ee82991499459271d72
Azzam Alsharafi <32015536+AzzamAlsharafi@users.noreply.github.com>
2024-03-05 09:21:15 +0000

    Fix API CORS isssue and make 'ports' fields consistent

--- src/api/rest.py
+++ src/api/rest.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from typing import List
 
 import uvicorn
 
 app = FastAPI()
+
+origins = [
+    '*'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 topology = {
     "devices": [],
